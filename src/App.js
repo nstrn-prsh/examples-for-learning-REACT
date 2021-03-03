fd39f6575be98133425d1ce7cf3ff4f000baf214
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-03-03 13:44:57 +0330

    a10- sticky note html css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     return (
+          <ul>
+               <li>
+                    <a href='#' contenteditable>
+                         <h2>Title #1</h2>
+                         <p>Text Content #1</p>
+                    </a>
+               </li>
+               <li>
+                    <a href='#' contenteditable>
+                         <h2>Title #2</h2>
+                         <p>Text Content #2</p>
+                    </a>
+               </li>
+               <li>
+                    <a href='#' contenteditable>
+                         <h2>Title #3</h2>
+                         <p>Text Content #3</p>
+                    </a>
+               </li>
+               <li>
+                    <a href='#' contenteditable>
+                         <h2>Title #4</h2>
+                         <p>Text Content #4</p>
+                    </a>
+               </li>
+               <li>
+                    <a href='#' contenteditable>
+                         <h2>Title #5</h2>
+                         <p>Text Content #5</p>
+                    </a>
+               </li>
+               <li>
+                    <a href='#' contenteditable>
+                         <h2>Title #6</h2>
+                         <p>Text Content #6</p>
+                    </a>
+               </li>
+               <li>
+                    <a href='#' contenteditable>
+                         <h2>Title #7</h2>
+                         <p>Text Content #7</p>
+                    </a>
+               </li>
+               <li>
+                    <a href='#' contenteditable>
+                         <h2>Title #8</h2>
+                         <p>Text Content #8</p>
+                    </a>
+               </li>
+          </ul>
+     );
 }
 
 export default App;
+
+/*
+$(document).ready(function () {
+  all_notes = $("li a");
+
+  all_notes.on("keyup", function () {
+    note_title = $(this).find("h2").text();
+    note_content = $(this).find("p").text();
+
+    item_key = "list_" + $(this).parent().index();
+
+    data = {
+      title: note_title,
+      content: note_content
+    };
+
+    window.localStorage.setItem(item_key, JSON.stringify(data));
+  });
+
+  all_notes.each(function (index) {
+    data = JSON.parse(window.localStorage.getItem("list_" + index));
+
+    if (data !== null) {
+      note_title = data.title;
+      note_content = data.content;
+
+      $(this).find("h2").text(note_title);
+      $(this).find("p").text(note_content);
+    }
+  });
+});
+*/
