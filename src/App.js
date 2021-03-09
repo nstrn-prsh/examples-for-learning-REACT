5d6b3192bfa8d5784dba038b974c797df562340e
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-03-09 22:58:47 +0330

    a14

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Fragment } from "react";
 import Clock from "./Components/Clock/Clock";
 import StickyNote from "./Components/StickyNote/StickyNote";
 import OnClick from "./Components/Event/OnClick";
+import Box from "./Components/Box/Box";
 
 function App() {
      return (
           <Fragment>
-               <Clock />
-               <StickyNote />
-               <OnClick />
+               {/* <Clock /> */}
+               {/* <StickyNote /> */}
+               {/* <OnClick /> */}
+               <Box />
           </Fragment>
      );
 }
