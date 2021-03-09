7a98ad9c4572b19df3f71ad3d86b41de4a8be90f
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-03-09 13:37:21 +0330

    a13

--- src/Components/Event/OnClick.jsx
+++ src/Components/Event/OnClick.jsx
@@ -1,25 +1,33 @@
 import React, { Fragment, Component } from "react";
 
 class OnClick extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-             counter: 0,
-        };
-        this.clickHandler=this.clickHandler.bind(this)
-    }
+     constructor(props) {
+          super(props);
+          this.state = {
+               counter: 0,
+          };
+          this.clickHandler = this.clickHandler.bind(this);
+     }
 
-    //  clickHandler = (event) => {
-    //       this.setState((state) => {
-    //            return { counter: state.counter + 1 };
-    //       });
-    //  };
+     //  clickHandler = (event) => {
+     //       this.setState((state) => {
+     //            return { counter: state.counter + 1 };
+     //       });
+     //  };
 
-    clickHandler(event){
+     clickHandler(event) {
+          console.log(event.altKey);
+          event.persist();
+          setTimeout(function () {
+               console.log(event.altKey);
+          }, 500);
+
+          console.log(event.target);
+
           this.setState((state) => {
                return { counter: state.counter + 1 };
           });
-     };
+     }
 
      render() {
           return (
