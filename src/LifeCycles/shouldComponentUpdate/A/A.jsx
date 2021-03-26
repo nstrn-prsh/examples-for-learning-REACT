8dc0bdaa8fbee6f433fc4c2fae0f6852d27dedcd
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-03-26 15:47:09 +0430

    pureComponent

--- src/LifeCycles/shouldComponentUpdate/A/A.jsx
+++ src/LifeCycles/shouldComponentUpdate/A/A.jsx
@@ -1,16 +1,31 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import color from "./../randomColor";
+import B1 from "./../B/B1";
+import C1 from "./../C/C1";
+import D1 from "./../D/D1";
+import C2 from "./../C/C2";
+import D2 from "./../D/D2";
+import B2 from "./../B/B2";
+import C4 from "./../C/C4";
+import C3 from "./../C/C3";
 
-export default class A extends Component {
-     shouldComponentUpdate(prevProps) {
-          if (this.props.number !== prevProps.number) return true;
-          return false;
-     }
-
+export default class A extends PureComponent {
      render() {
           return (
                <div style={{ backgroundColor: color() }}>
-                    A{this.props.children}
+                    A
+                    <B1>
+                         <C1>
+                              <D1></D1>
+                         </C1>
+                         <C2>
+                              <D2></D2>
+                         </C2>
+                    </B1>
+                    <B2>
+                         <C3></C3>
+                         <C4></C4>
+                    </B2>
                </div>
           );
      }
--- src/LifeCycles/shouldComponentUpdate/Root.jsx
+++ src/LifeCycles/shouldComponentUpdate/Root.jsx
@@ -2,14 +2,6 @@ import React, { Component } from "react";
 import A from "./A/A";
 import color from "./randomColor";
 import "./root.css";
-import B1 from "./B/B1";
-import B2 from "./B/B2";
-import C1 from "./C/C1";
-import C2 from "./C/C2";
-import C3 from "./C/C3";
-import C4 from "./C/C4";
-import D1 from "./D/D1";
-import D2 from "./D/D2";
 
 export default class Root extends Component {
      constructor(props) {
@@ -33,35 +25,8 @@ export default class Root extends Component {
                          backgroundColor: color(),
                     }}
                >
-                    <A number={this.state.number}>
-                         <B1>
-                              <C1>
-                                   <D1></D1>
-                              </C1>
-                              <C2>
-                                   <D2></D2>
-                              </C2>
-                         </B1>
-                         <B2>
-                              <C3></C3>
-                              <C4></C4>
-                         </B2>
-                    </A>
-
-                    <A>
-                         <B1>
-                              <C1>
-                                   <D1></D1>
-                              </C1>
-                              <C2>
-                                   <D2></D2>
-                              </C2>
-                         </B1>
-                         <B2>
-                              <C3></C3>
-                              <C4></C4>
-                         </B2>
-                    </A>
+                    <A number={this.state.number} />
+                    <A />
                     <button onClick={this.generateRandomNumber}>
                          Generate Random props
                     </button>
