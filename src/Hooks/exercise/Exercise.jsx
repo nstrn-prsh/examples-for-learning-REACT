5a53660e7337cb0f4ff6b9c858e374dceeccb9fd
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-04-06 15:33:55 +0430

    c17: user actions

--- src/Hooks/exercise/Exercise.jsx
+++ src/Hooks/exercise/Exercise.jsx
@@ -11,22 +11,39 @@ const Exercise = () => {
      const [getPostId, setPostId] = useState(1);
      const [getLoading, setLoading] = useState(true);
 
+     const handleState = (type, payload) => {
+          switch (type) {
+               case "GET_ANSWER_SUCCESS":
+                    setTitle(payload.title);
+                    setLoading(false);
+                    setToastify({
+                         type: "success",
+                         msg: payload.msg,
+                    });
+                    break;
+               case "SEND_REQUEST":
+                    setPostId(payload);
+                    setLoading(true);
+                    break;
+
+               default:
+                    break;
+          }
+     };
+
      useEffect(() => {
           fetch(`https://jsonplaceholder.typicode.com/posts/${getPostId}`)
                .then((res) => res.json())
                .then((post) => {
-                    setTitle(post.title);
-                    setLoading(false);
-                    setToastify({
-                         type: "success",
+                    handleState("GET_ANSWER_SUCCESS", {
+                         title: post.title,
                          msg: `post ${getPostId} loaded!`,
                     });
                });
      }, [getPostId]);
 
      const handleId = (e) => {
-          setPostId(e.target.value);
-          setLoading(true);
+          handleState("SEND_REQUEST", e.target.value);
      };
 
      return (
