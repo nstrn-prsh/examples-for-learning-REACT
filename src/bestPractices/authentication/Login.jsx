cfbf725006220b88e06e247cd3d7d4f790a974a7
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-06-23 05:04:35 +0430

    d11- ba refresh login bemoone

--- src/bestPractices/authentication/Login.jsx
+++ src/bestPractices/authentication/Login.jsx
@@ -1,25 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useLayoutEffect } from "react";
 import axios from "axios";
-import { useAuthState } from "./context/auth-context";
+import { useAuthDispatch, useAuthState } from "./context/auth-context";
 import { actionTypes } from "./context/reducer";
 
-const fetchToken = async (userName, password) => {
+const fetchToken = async (username, password) => {
      return axios
           .post("http://localhost:3001/login", {
-               userName,
+               username,
                password,
           })
-          .then((res) => res.data);
+          .then((response) => response.data);
 };
 
 const fetchCurrentUserInfo = (token) => {
      return axios
-          .get("http://localhost:3001/login/users/me", {
+          .get("http://localhost:3001/users/me", {
                headers: {
                     authorization: token,
                },
           })
-          .then((res) => res.data);
+          .then((response) => response.data);
 };
 
 export default function Login() {
@@ -27,22 +27,38 @@ export default function Login() {
      const [getPassword, setPassword] = useState("");
      const [token, setToken] = useState(null);
 
-     const dispatch = useAuthState();
+     const dispatch = useAuthDispatch();
+     const { loading } = useAuthState();
 
      const handleLogin = (e) => {
           e.preventDefault();
-          fetchToken(getUserName, getPassword).then((success, data) => {
+          dispatch({
+               type: actionTypes.LOGIN_REQUEST,
+          });
+          fetchToken(getUserName, getPassword).then(({ success, data }) => {
                if (success) {
                     setToken(data);
                }
           });
      };
 
+     // check mikonim ke token darim ya na
+     useLayoutEffect(() => {
+          const token = localStorage.getItem("token");
+          if (token) {
+               dispatch({
+                    type: actionTypes.LOGIN_REQUEST,
+               });
+               setToken(token);
+          }
+     }, [dispatch]);
+
      //  harzaman ke token taghiir kar ye request samte server beferestim va etelaate karbar ro begirim
      useEffect(() => {
           if (token) {
                fetchCurrentUserInfo(token).then(({ success, data }) => {
                     if (success) {
+                         localStorage.setItem("token", token);
                          dispatch({
                               type: actionTypes.LOGIN_SUCCESS,
                               payload: {
@@ -56,30 +72,36 @@ export default function Login() {
      }, [token, dispatch]);
 
      return (
-          <div className='login'>
-               <h1>Login</h1>
-               <form method='post' onSubmit={handleLogin}>
-                    <input
-                         type='text'
-                         value={getUserName}
-                         onChange={(e) => setUserName(e.target.value)}
-                         placeholder='Username'
-                         required='required'
-                    />
-                    <input
-                         type='password'
-                         value={getPassword}
-                         onChange={(e) => setPassword(e.target.value)}
-                         placeholder='Password'
-                         required='required'
-                    />
-                    <button
-                         type='submit'
-                         className='btn btn-primary btn-block btn-large'
-                    >
-                         Let me in.
-                    </button>
-               </form>
-          </div>
+          <>
+               {loading ? (
+                    <p>loading</p>
+               ) : (
+                    <div className='login'>
+                         <h1>Login</h1>
+                         <form method='post' onSubmit={handleLogin}>
+                              <input
+                                   type='text'
+                                   value={getUserName}
+                                   onChange={(e) => setUserName(e.target.value)}
+                                   placeholder='Username'
+                                   required='required'
+                              />
+                              <input
+                                   type='password'
+                                   value={getPassword}
+                                   onChange={(e) => setPassword(e.target.value)}
+                                   placeholder='Password'
+                                   required='required'
+                              />
+                              <button
+                                   type='submit'
+                                   className='btn btn-primary btn-block btn-large'
+                              >
+                                   Let me in.
+                              </button>
+                         </form>
+                    </div>
+               )}
+          </>
      );
 }
